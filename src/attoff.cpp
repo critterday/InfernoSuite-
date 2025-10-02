908978846702bf8f37ecbfe269bfa743f6d92681
critterday <tjons26@wooster.edu>
2025-10-02 13:07:46 -0400

    decent panel graphic

---
+++ src/attoff.cpp
@@ -0,0 +1,185 @@
+#include "plugin.hpp"
+
+
+struct ATTOFF : Module {
+	enum ParamId {
+		OFF1_PARAM,
+		AFF1_PARAM,
+		OFF2_PARAM,
+		AFF2_PARAM,
+		OFF3_PARAM,
+		AFF3_PARAM,
+		OFF4_PARAM,
+		AFF4_PARAM,
+		OFF5_PARAM,
+		AFF5_PARAM,
+		OFF6_PARAM,
+		AFF6_PARAM,
+		OFF7_PARAM,
+		AFF7_PARAM,
+		OFF8_PARAM,
+		AFF8_PARAM,
+		PARAMS_LEN
+	};
+	enum InputId {
+		IN1_INPUT,
+		IN2_INPUT,
+		IN3_INPUT,
+		IN4_INPUT,
+		IN5_INPUT,
+		IN6_INPUT,
+		IN7_INPUT,
+		IN8_INPUT,
+		INPUTS_LEN
+	};
+	enum OutputId {
+		OUT1_OUTPUT,
+		OUT2_OUTPUT,
+		OUT3_OUTPUT,
+		OUT4_OUTPUT,
+		OUT5_OUTPUT,
+		OUT6_OUTPUT,
+		OUT7_OUTPUT,
+		OUT8_OUTPUT,
+		OUTPUTS_LEN
+	};
+	enum LightId {
+		LIGHTS_LEN
+	};
+
+	ATTOFF() {
+		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
+		configParam(OFF1_PARAM, -5.f, 5.f, 0.f, "Channel 1 Offset");
+		configParam(AFF1_PARAM, -1.f, 1.f, 0.f, "Channel 1 Attenuvert");
+		configParam(OFF2_PARAM, -5.f, 5.f, 0.f, "Channel 2 Offset");
+		configParam(AFF2_PARAM, -1.f, 1.f, 0.f, "Channel 2 Attenuvert");
+		configParam(OFF3_PARAM, -5.f, 5.f, 0.f, "Channel 3 Offset");
+		configParam(AFF3_PARAM, -1.f, 1.f, 0.f, "Channel 3 Attenuvert");
+		configParam(OFF4_PARAM, -5.f, 5.f, 0.f, "Channel 4 Offset");
+		configParam(AFF4_PARAM, -1.f, 1.f, 0.f, "Channel 4 Attenuvert");
+		configParam(OFF5_PARAM, -5.f, 5.f, 0.f, "Channel 5 Offset");
+		configParam(AFF5_PARAM, -1.f, 1.f, 0.f, "Channel 5 Attenuvert");
+		configParam(OFF6_PARAM, -5.f, 5.f, 0.f, "Channel 6 Offset");
+		configParam(AFF6_PARAM, -1.f, 1.f, 0.f, "Channel 6 Attenuvert");
+		configParam(OFF7_PARAM, -5.f, 5.f, 0.f, "Channel 7 Offset");
+		configParam(AFF7_PARAM, -1.f, 1.f, 0.f, "Channel 7 Attenuvert");
+		configParam(OFF8_PARAM, -5.f, 5.f, 0.f, "Channel 8 Offset");
+		configParam(AFF8_PARAM, -1.f, 1.f, 0.f, "Channel 8 Attenuvert");
+		configInput(IN1_INPUT, "Channel 1");
+		configInput(IN2_INPUT, "Channel 2");
+		configInput(IN3_INPUT, "Channel 3");
+		configInput(IN4_INPUT, "Channel 4");
+		configInput(IN5_INPUT, "Channel 5");
+		configInput(IN6_INPUT, "Channel 6");
+		configInput(IN7_INPUT, "Channel 7");
+		configInput(IN8_INPUT, "Channel 8");
+		configOutput(OUT1_OUTPUT, "Channel 1");
+		configOutput(OUT2_OUTPUT, "Channel 2");
+		configOutput(OUT3_OUTPUT, "Channel 3");
+		configOutput(OUT4_OUTPUT, "Channel 4");
+		configOutput(OUT5_OUTPUT, "Channel 5");
+		configOutput(OUT6_OUTPUT, "Channel 6");
+		configOutput(OUT7_OUTPUT, "Channel 7");
+		configOutput(OUT8_OUTPUT, "Channel 8");
+	}
+
+	void process(const ProcessArgs& args) override {
+
+		// get input voltages 
+
+		float involt1 = inputs[IN1_INPUT].getVoltage();
+		float involt2 = inputs[IN2_INPUT].getVoltage();
+		float involt3 = inputs[IN3_INPUT].getVoltage();
+		float involt4 = inputs[IN4_INPUT].getVoltage();
+		float involt5 = inputs[IN5_INPUT].getVoltage();
+		float involt6 = inputs[IN6_INPUT].getVoltage();
+		float involt7 = inputs[IN7_INPUT].getVoltage();
+		float involt8 = inputs[IN8_INPUT].getVoltage();
+
+		// compute attenuversion voltages
+
+		float att1 = involt1 * params[AFF1_PARAM].getValue();
+		float att2 = involt2 * params[AFF2_PARAM].getValue();
+		float att3 = involt3 * params[AFF3_PARAM].getValue();
+		float att4 = involt4 * params[AFF4_PARAM].getValue();
+		float att5 = involt5 * params[AFF5_PARAM].getValue();
+		float att6 = involt6 * params[AFF6_PARAM].getValue();
+		float att7 = involt7 * params[AFF7_PARAM].getValue();
+		float att8 = involt8 * params[AFF8_PARAM].getValue();
+
+		// compute offsets
+
+		float off1 = att1 + params[OFF1_PARAM].getValue();
+		float off2 = att2 + params[OFF2_PARAM].getValue();
+		float off3 = att3 + params[OFF3_PARAM].getValue();
+		float off4 = att4 + params[OFF4_PARAM].getValue();
+		float off5 = att5 + params[OFF5_PARAM].getValue();
+		float off6 = att6 + params[OFF6_PARAM].getValue();
+		float off7 = att7 + params[OFF7_PARAM].getValue();
+		float off8 = att8 + params[OFF8_PARAM].getValue();
+
+		// output
+		
+		
+		outputs[OUT1_OUTPUT].setVoltage(off1);
+		outputs[OUT2_OUTPUT].setVoltage(off2);
+		outputs[OUT3_OUTPUT].setVoltage(off3);
+		outputs[OUT4_OUTPUT].setVoltage(off4);
+		outputs[OUT5_OUTPUT].setVoltage(off5);
+		outputs[OUT6_OUTPUT].setVoltage(off6);
+		outputs[OUT7_OUTPUT].setVoltage(off7);
+		outputs[OUT8_OUTPUT].setVoltage(off8);
+
+	}
+};
+
+
+struct ATTOFFWidget : ModuleWidget {
+	ATTOFFWidget(ATTOFF* module) {
+		setModule(module);
+		setPanel(createPanel(asset::plugin(pluginInstance, "res/ATTOFF.svg")));
+
+		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
+		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
+		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(40.176, 19.425)), module, ATTOFF::OFF1_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(19.887, 19.69)), module, ATTOFF::AFF1_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(39.61, 32.351)), module, ATTOFF::OFF2_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(19.51, 32.615)), module, ATTOFF::AFF2_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(39.798, 42.446)), module, ATTOFF::OFF3_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(18.896, 42.71)), module, ATTOFF::AFF3_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(39.893, 54.711)), module, ATTOFF::OFF4_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(18.99, 54.975)), module, ATTOFF::AFF4_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(39.893, 67.919)), module, ATTOFF::OFF5_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(18.99, 68.184)), module, ATTOFF::AFF5_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(39.987, 81.411)), module, ATTOFF::OFF6_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(19.085, 81.676)), module, ATTOFF::AFF6_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(40.176, 94.997)), module, ATTOFF::OFF7_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(19.274, 95.262)), module, ATTOFF::AFF7_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(40.081, 108.583)), module, ATTOFF::OFF8_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(19.179, 108.848)), module, ATTOFF::AFF8_PARAM));
+
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(5.849, 19.624)), module, ATTOFF::IN1_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(5.283, 32.55)), module, ATTOFF::IN2_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(5.472, 42.645)), module, ATTOFF::IN3_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(5.566, 54.91)), module, ATTOFF::IN4_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(5.566, 68.118)), module, ATTOFF::IN5_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(5.661, 81.61)), module, ATTOFF::IN6_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(5.849, 95.196)), module, ATTOFF::IN7_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(5.755, 108.782)), module, ATTOFF::IN8_INPUT));
+
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(54.992, 19.425)), module, ATTOFF::OUT1_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(54.426, 32.351)), module, ATTOFF::OUT2_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(54.615, 42.446)), module, ATTOFF::OUT3_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(54.709, 54.711)), module, ATTOFF::OUT4_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(54.709, 67.919)), module, ATTOFF::OUT5_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(54.804, 81.411)), module, ATTOFF::OUT6_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(54.992, 94.997)), module, ATTOFF::OUT7_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(54.898, 108.583)), module, ATTOFF::OUT8_OUTPUT));
+	}
+};
+
+
+Model* modelATTOFF = createModel<ATTOFF, ATTOFFWidget>("ATTOFF");
---
+++ src/supply.cpp
@@ -0,0 +1,251 @@
+#include "plugin.hpp"
+
+
+struct SUPPLY : Module {
+	enum ParamId {
+		C1P2_PARAM,
+		C1P4_PARAM,
+		C1P3_PARAM,
+		C1P1_PARAM,
+		C1B_PARAM,
+		C2P4_PARAM,
+		C2P3_PARAM,
+		C2P1_PARAM,
+		C2P2_PARAM,
+		C2B_PARAM,
+		C3P3_PARAM,
+		C3P1_PARAM,
+		C3P2_PARAM,
+		C3P4_PARAM,
+		C3B_PARAM,
+		C4P2_PARAM,
+		C4P1_PARAM,
+		C4B_PARAM,
+		C4P3_PARAM,
+		C4P4_PARAM,
+		C5P2_PARAM,
+		C5P3_PARAM,
+		C5P4_PARAM,
+		C5P1_PARAM,
+		C5B_PARAM,
+		C6P2_PARAM,
+		C6P1_PARAM,
+		C6P4_PARAM,
+		C6B_PARAM,
+		C6P3_PARAM,
+		C7P1_PARAM,
+		C7P3_PARAM,
+		C7B_PARAM,
+		C7P4_PARAM,
+		C7P2_PARAM,
+		C8P3_PARAM,
+		C8P1_PARAM,
+		C8P2_PARAM,
+		C8P4_PARAM,
+		C8B_PARAM,
+		PARAMS_LEN
+	};
+	enum InputId {
+		GATE1_INPUT,
+		GATE2_INPUT,
+		GATE3_INPUT,
+		GATE4_INPUT,
+		GATE5_INPUT,
+		GATE6_INPUT,
+		GATE7_INPUT,
+		GATE8_INPUT,
+		INPUTS_LEN
+	};
+	enum OutputId {
+		OUT1_OUTPUT,
+		OUT4_OUTPUT,
+		OUT3_OUTPUT,
+		OUT2_OUTPUT,
+		OUTPUTS_LEN
+	};
+	enum LightId {
+		LIGHT1_LIGHT,
+		LIGHT2_LIGHT,
+		LIGHT3_LIGHT,
+		LIGHT4_LIGHT,
+		LIGHT5_LIGHT,
+		LIGHT6_LIGHT,
+		LIGHT7_LIGHT,
+		LIGHT8_LIGHT,
+		LIGHTS_LEN
+	};
+
+	SUPPLY() {
+		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
+		configParam(C1P2_PARAM, -10.f, 10.f, 0.f, "");
+		configParam(C1P4_PARAM, -10.f, 10.f, 0.f, "");
+		configParam(C1P3_PARAM, -10.f, 10.f, 0.f, "");
+		configParam(C1P1_PARAM, -10.f, 10.f, 0.f, "");
+		configParam(C1B_PARAM, 0.f, 1.f, 0.f, "");
+		configParam(C2P4_PARAM, -10.f, 10.f, 0.f, "");
+		configParam(C2P3_PARAM, -10.f, 10.f, 0.f, "");
+		configParam(C2P1_PARAM, -10.f, 10.f, 0.f, "");
+		configParam(C2P2_PARAM, -10.f, 10.f, 0.f, "");
+		configParam(C2B_PARAM, 0.f, 1.f, 0.f, "");
+		configParam(C3P3_PARAM, -10.f, 10.f, 0.f, "");
+		configParam(C3P1_PARAM, -10.f, 10.f, 0.f, "");
+		configParam(C3P2_PARAM, -10.f, 10.f, 0.f, "");
+		configParam(C3P4_PARAM, -10.f, 10.f, 0.f, "");
+		configParam(C3B_PARAM, 0.f, 1.f, 0.f, "");
+		configParam(C4P2_PARAM, -10.f, 10.f, 0.f, "");
+		configParam(C4P1_PARAM, -10.f, 10.f, 0.f, "");
+		configParam(C4B_PARAM, 0.f, 1.f, 0.f, "");
+		configParam(C4P3_PARAM, -10.f, 10.f, 0.f, "");
+		configParam(C4P4_PARAM, -10.f, 10.f, 0.f, "");
+		configParam(C5P2_PARAM, -10.f, 10.f, 0.f, "");
+		configParam(C5P3_PARAM, -10.f, 10.f, 0.f, "");
+		configParam(C5P4_PARAM, -10.f, 10.f, 0.f, "");
+		configParam(C5P1_PARAM, -10.f, 10.f, 0.f, "");
+		configParam(C5B_PARAM, 0.f, 1.f, 0.f, "");
+		configParam(C6P2_PARAM, -10.f, 10.f, 0.f, "");
+		configParam(C6P1_PARAM, -10.f, 10.f, 0.f, "");
+		configParam(C6P4_PARAM, -10.f, 10.f, 0.f, "");
+		configParam(C6B_PARAM, 0.f, 1.f, 0.f, "");
+		configParam(C6P3_PARAM, -10.f, 10.f, 0.f, "");
+		configParam(C7P1_PARAM, -10.f, 10.f, 0.f, "");
+		configParam(C7P3_PARAM, -10.f, 10.f, 0.f, "");
+		configParam(C7B_PARAM, 0.f, 10.f, 0.f, "");
+		configParam(C7P4_PARAM, -10.f, 10.f, 0.f, "");
+		configParam(C7P2_PARAM, -10.f, 10.f, 0.f, "");
+		configParam(C8P3_PARAM, -10.f, 10.f, 0.f, "");
+		configParam(C8P1_PARAM, -10.f, 10.f, 0.f, "");
+		configParam(C8P2_PARAM, -10.f, 10.f, 0.f, "");
+		configParam(C8P4_PARAM, -10.f, 10.f, 0.f, "");
+		configParam(C8B_PARAM, 0.f, 1.f, 0.f, "");
+		configInput(GATE1_INPUT, "");
+		configInput(GATE2_INPUT, "");
+		configInput(GATE3_INPUT, "");
+		configInput(GATE4_INPUT, "");
+		configInput(GATE5_INPUT, "");
+		configInput(GATE6_INPUT, "");
+		configInput(GATE7_INPUT, "");
+		configInput(GATE8_INPUT, "");
+		configOutput(OUT1_OUTPUT, "");
+		configOutput(OUT4_OUTPUT, "");
+		configOutput(OUT3_OUTPUT, "");
+		configOutput(OUT2_OUTPUT, "");
+	}
+
+	void process(const ProcessArgs& args) override {
+	}
+};
+
+
+struct SUPPLYWidget : ModuleWidget {
+	SUPPLYWidget(SUPPLY* module) {
+		setModule(module);
+		setPanel(createPanel(asset::plugin(pluginInstance, "res/SUPPLY.svg")));
+
+		// screws
+
+		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
+		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
+		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+
+		// selection buttons
+		addParam(createParamCentered<VCVButton>(mm2px(Vec(15.75, 28.5)), module, SUPPLY::C1B_PARAM));
+		addParam(createParamCentered<VCVButton>(mm2px(Vec(15.75, 40)), module, SUPPLY::C2B_PARAM));
+		addParam(createParamCentered<VCVButton>(mm2px(Vec(15.75, 51.5)), module, SUPPLY::C3B_PARAM));
+		addParam(createParamCentered<VCVButton>(mm2px(Vec(15.75, 63)), module, SUPPLY::C4B_PARAM));
+		addParam(createParamCentered<VCVButton>(mm2px(Vec(15.75, 74.5)), module, SUPPLY::C5B_PARAM));
+		addParam(createParamCentered<VCVButton>(mm2px(Vec(15.75, 86)), module, SUPPLY::C6B_PARAM));
+		addParam(createParamCentered<VCVButton>(mm2px(Vec(15.75, 97.5)), module, SUPPLY::C7B_PARAM));
+		addParam(createParamCentered<VCVButton>(mm2px(Vec(15.75, 109)), module, SUPPLY::C8B_PARAM));
+
+		// voltage dials
+
+		// channel 1
+
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(43, 28.5)), module, SUPPLY::C1P2_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(64, 28.5)), module, SUPPLY::C1P4_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(53.5, 28.5)), module, SUPPLY::C1P3_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(32.305, 28.5)), module, SUPPLY::C1P1_PARAM));
+
+		// channel 2
+		
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(64, 40)), module, SUPPLY::C2P4_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(53.5, 40)), module, SUPPLY::C2P3_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(32.305, 40)), module, SUPPLY::C2P1_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(43, 40)), module, SUPPLY::C2P2_PARAM));
+
+		// channel 3
+		
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(53.5, 51.5)), module, SUPPLY::C3P3_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(32.305, 51.5)), module, SUPPLY::C3P1_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(43, 51.5)), module, SUPPLY::C3P2_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(64, 51.5)), module, SUPPLY::C3P4_PARAM));
+
+		// channel 4
+		
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(43, 63)), module, SUPPLY::C4P2_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(32.305, 63)), module, SUPPLY::C4P1_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(53.5, 63)), module, SUPPLY::C4P3_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(64, 63)), module, SUPPLY::C4P4_PARAM));
+
+		// channel 5
+
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(43, 74.5)), module, SUPPLY::C5P2_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(53.5, 74.5)), module, SUPPLY::C5P3_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(64, 74.5)), module, SUPPLY::C5P4_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(32.305, 74.5)), module, SUPPLY::C5P1_PARAM));
+
+		// channel 6
+		
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(43, 86)), module, SUPPLY::C6P2_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(32.305, 86)), module, SUPPLY::C6P1_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(64, 86)), module, SUPPLY::C6P4_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(53.5, 86)), module, SUPPLY::C6P3_PARAM));
+
+		// channel 7
+
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(32.305, 97.5)), module, SUPPLY::C7P1_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(53.5, 97.5)), module, SUPPLY::C7P3_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(64, 97.5)), module, SUPPLY::C7P4_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(43, 97.5)), module, SUPPLY::C7P2_PARAM));
+
+		// channel 8
+
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(53.5, 109)), module, SUPPLY::C8P3_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(32.305, 109)), module, SUPPLY::C8P1_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(43, 109)), module, SUPPLY::C8P2_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(64, 109)), module, SUPPLY::C8P4_PARAM));
+		
+		// select gate inputs
+
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(6.75, 28.5)), module, SUPPLY::GATE1_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(6.75, 40)), module, SUPPLY::GATE2_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(6.75, 51.5)), module, SUPPLY::GATE3_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(6.75, 63)), module, SUPPLY::GATE4_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(6.75, 74.5)), module, SUPPLY::GATE5_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(6.75, 86)), module, SUPPLY::GATE6_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(6.75, 97.5)), module, SUPPLY::GATE7_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(6.75, 109)), module, SUPPLY::GATE8_INPUT));
+
+		// voltage outputs
+
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(23, 120)), module, SUPPLY::OUT1_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(59, 120)), module, SUPPLY::OUT4_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(47, 120)), module, SUPPLY::OUT3_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(35, 120)), module, SUPPLY::OUT2_OUTPUT));
+
+		// indicator lights
+
+		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(23.285, 28.5)), module, SUPPLY::LIGHT1_LIGHT));
+		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(23.285, 40)), module, SUPPLY::LIGHT2_LIGHT));
+		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(23.285, 51.5)), module, SUPPLY::LIGHT3_LIGHT));
+		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(23.285, 63)), module, SUPPLY::LIGHT4_LIGHT));
+		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(23.285, 74.5)), module, SUPPLY::LIGHT5_LIGHT));
+		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(23.285, 86)), module, SUPPLY::LIGHT6_LIGHT));
+		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(23.285, 97.5)), module, SUPPLY::LIGHT7_LIGHT));
+		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(23.285, 109)), module, SUPPLY::LIGHT8_LIGHT));
+	}
+};
+
+
+Model* modelSUPPLY = createModel<SUPPLY, SUPPLYWidget>("SUPPLY");
